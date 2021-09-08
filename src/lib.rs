3e06a92b6b24edad05dc08e49f11915e473ec6e1
“suchapalaver” <jlivesey@gmail.com>
2021-09-08 15:08:58 -0400

    implemented collect() iterator

--- src/lib.rs
+++ src/lib.rs
@@ -44,9 +44,11 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 
     let reader = fasta::Reader::from_file(&filepath).unwrap();
 
-    reader.records().into_par_iter().for_each(|result| {
+    let fasta_records: Vec<Result<fasta::Record, std::io::Error>> = reader.records().collect();
+
+    fasta_records.par_iter().for_each(|result| {
 	
-        let result_data = result.unwrap();
+        let result_data = result.as_ref().unwrap();
 
         let pathname = format!("output/{}.tsv", result_data.id());
 
@@ -76,68 +78,3 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 
     Ok(println!("{}", filepath))
 }
-
-/*
-    for result in reader.records() {
-
-    let t = thread::spawn( move || {
-
-            let result_data = &result.unwrap();
-
-        let pathname = format!("output/{}.tsv", result_data.id());
-
-        let path = Path::new(&pathname);
-
-        let display = path.display();
-
-        let mut file = match File::create(&path) {
-
-        Err(why) => panic!("couldn't create {}: {}", display, why),
-        Ok(file) => file,
-        };
-        for (kmer, kmer_positions) in hash_kmers(result_data.seq(), kmer_len) {
-*/
-
-//  trying to think about a better way to do
-//  this than the nested match patterns below.
-//  Here's immeidately below commented out is
-//  one attempt that compiles but this is from
-//  before I added print to file capability:
-/*
-       let kmer_s = str::from_utf8(&kmer).unwrap();
-       let rvc = revcomp(kmer);
-       let rvc_s = str::from_utf8(&rvc).unwrap();
-       println!("{}\t{}\t{}\t{}", result_data.id(), kmer_s, rvc_s, kmer_positions.len());
-       }
-       });
-*/
-/*
-        let rvc = revcomp(kmer);
-
-        match str::from_utf8(kmer) {
-
-            Err(e) => println!("Problem: {}", e),
-            Ok(kmer_s) => {
-
-            match str::from_utf8(&rvc) {
-
-                Ok(rvc) => {
-
-                let data = format!("{}\t{}\t{}\n", kmer_s, rvc, kmer_positions.len());
-                write!(file, "{}", data).expect("Unable to write file");
-                }
-                Err(why) => panic!("couldn't write to {}: {}", display, why),
-            }
-            }
-        }
-        }
-    });
-    threads.push(t);
-    }
-    for t in threads {
-
-    t.join().expect("thread failed");
-    }
-    Ok(println!("{}", filepath))
-}
-*/
