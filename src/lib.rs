6e5399f42424911f8e763fc28a490cb1df273712
“suchapalaver” <jlivesey@gmail.com>
2021-09-10 14:59:54 -0400

    collect to Vec as main

--- src/lib.rs
+++ src/lib.rs
@@ -12,10 +12,7 @@ use bio::{alignment::sparse::hash_kmers,
 	  io::fasta
 };
 extern crate rayon;
-use rayon::{iter::ParallelBridge,
-	    prelude::ParallelIterator
-};
-use std::time::Instant;
+use rayon::prelude::*;
 
 pub struct Config {
     pub kmer_len: String,
@@ -25,13 +22,8 @@ pub struct Config {
 impl Config {
     
     pub fn new(mut args: env::Args) -> Result<Config, &'static str> {
-<<<<<<< HEAD
-
-	args.next();
-=======
 	
         args.next();
->>>>>>> collect_the_iterator
 
         let kmer_len = match args.next() {
 	    
@@ -48,7 +40,6 @@ impl Config {
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let start = Instant::now();
     
     fs::create_dir("output")?;
 
@@ -58,13 +49,9 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 
     let reader = fasta::Reader::from_file(&filepath).unwrap();
 
-<<<<<<< HEAD
-    reader.records().par_bridge().for_each(|result| {
-=======
     let fasta_records: Vec<Result<fasta::Record, std::io::Error>> = reader.records().collect();
 
     fasta_records.par_iter().for_each(|result| {
->>>>>>> collect_the_iterator
 	
         let result_data = result.as_ref().unwrap();
 
@@ -81,23 +68,18 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
         };
 	
         for (kmer, kmer_positions) in hash_kmers(result_data.seq(), kmer_len) {
-<<<<<<< HEAD
 
-	    let rvc = revcomp(kmer);
-=======
-	    
             let rvc = revcomp(kmer);
->>>>>>> collect_the_iterator
 
             match str::from_utf8(kmer) {
 		
                 Err(e) => println!("Problem: {}", e),
                 Ok(kmer_s) => match str::from_utf8(&rvc) {
 
-<<<<<<< HEAD
+
 		    Err(why) => panic!("couldn't write to {}: {}", display, why),
-=======
->>>>>>> collect_the_iterator
+
+
 		    Ok(rvc) => {
 
 			let data = format!("{}\t{}\t{}\n", kmer_s, rvc, kmer_positions.len());
@@ -107,10 +89,5 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
             }
         }
     });
-<<<<<<< HEAD
-    let duration = start.elapsed();
-    println!("Time elapsed is: {:?}", duration);
-=======
->>>>>>> collect_the_iterator
     Ok(println!("{}", filepath))
 }
