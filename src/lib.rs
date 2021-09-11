1f991ab574dfcf1aae6d785aaf25e78be8a74d7b
“suchapalaver” <jlivesey@gmail.com>
2021-09-11 09:04:27 -0400

    formatting main

--- src/lib.rs
+++ src/lib.rs
@@ -4,13 +4,10 @@ use std::{
     fs::{self, File},
     io::Write,
     path::Path,
-    str
+    str,
 };
 extern crate bio;
-use bio::{alignment::sparse::hash_kmers,
-	  alphabets::dna::revcomp,
-	  io::fasta
-};
+use bio::{alignment::sparse::hash_kmers, alphabets::dna::revcomp, io::fasta};
 extern crate rayon;
 use rayon::prelude::*;
 
@@ -20,18 +17,14 @@ pub struct Config {
 }
 
 impl Config {
-    
     pub fn new(mut args: env::Args) -> Result<Config, &'static str> {
-	
         args.next();
 
         let kmer_len = match args.next() {
-	    
             Some(arg) => arg,
             None => return Err("Didn't get a k-mer length"),
         };
         let filepath = match args.next() {
-	    
             Some(arg) => arg,
             None => return Err("Didn't get a file name"),
         };
@@ -40,7 +33,6 @@ impl Config {
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    
     fs::create_dir("output")?;
 
     let filepath: String = config.filepath;
@@ -52,7 +44,6 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let fasta_records: Vec<Result<fasta::Record, std::io::Error>> = reader.records().collect();
 
     fasta_records.par_iter().for_each(|result| {
-	
         let result_data = result.as_ref().unwrap();
 
         let pathname = format!("output/{}.tsv", result_data.id());
@@ -62,27 +53,20 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
         let display = path.display();
 
         let mut file = match File::create(&path) {
-	    
             Err(why) => panic!("couldn't create {}: {}", display, why),
             Ok(file) => file,
         };
-	
-        for (kmer, kmer_positions) in hash_kmers(result_data.seq(), kmer_len) {
 
+        for (kmer, kmer_positions) in hash_kmers(result_data.seq(), kmer_len) {
             let rvc = revcomp(kmer);
 
             match str::from_utf8(kmer) {
-		
                 Err(e) => println!("Problem: {}", e),
                 Ok(kmer_s) => match str::from_utf8(&rvc) {
+                    Err(why) => panic!("couldn't write to {}: {}", display, why),
 
-
-		    Err(why) => panic!("couldn't write to {}: {}", display, why),
-
-
-		    Ok(rvc) => {
-
-			let data = format!("{}\t{}\t{}\n", kmer_s, rvc, kmer_positions.len());
+                    Ok(rvc) => {
+                        let data = format!("{}\t{}\t{}\n", kmer_s, rvc, kmer_positions.len());
                         write!(file, "{}", data).expect("Unable to write file");
                     }
                 },
