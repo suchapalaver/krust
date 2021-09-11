454266d2523558aaa53dc0ac09daf3aab95d94e0
“suchapalaver” <jlivesey@gmail.com>
2021-09-11 09:14:31 -0400

    formatting main

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,16 @@ use std::process;
 use krust::Config;
 
 fn main() {
-   
     let config = Config::new(env::args()).unwrap_or_else(|err| {
         println!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
-    
+
     println!("\nSearching for kmers of length {}", config.kmer_len);
     println!("... in file {}\n", config.filepath);
-    
+
     if let Err(e) = krust::run(config) {
         println!("Application error: {}", e);
         process::exit(1);
     }
-}   
+}
