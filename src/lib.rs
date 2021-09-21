f0ac72b298ac5989fbc6827535875c87050ebe8b
“suchapalaver” <jlivesey@gmail.com>
2021-09-21 17:33:01 -0400

    working on merging

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
 use bio::{alignment::sparse::hash_kmers, alphabets::dna::revcomp, io::fasta};
+use itertools::Itertools;
 use rayon::prelude::*;
+use reduce::Reduce;
+//use rustc_hash::FxHashMap; //  Check Stack Overflow for suggestions
 use std::{collections::{HashMap, HashSet}, env, error::Error, fs::File, io::Write, str, time::Instant};
 
 pub struct Config {
@@ -20,7 +23,44 @@ impl Config {
         Ok(Config { kmer_len, filepath })
     }
 }
+/*
+pub fn div_n_merge(hash_vec_a: Vec<HashMap<&[u8], usize>>, hash_vec_b: Vec<HashMap<&[u8], usize>>) -> Vec<HashMap<&[u8], usize>> {
 
+    let mut a = Vec::new();
+    
+    let mut b = Vec::new();
+	
+    while hash_vec.len() > 1 {
+	if a.is_empty() | b.is_empty() { // is | correct here?
+	    
+	    let mid = hash_vec.len()/2;
+	    
+	    for i in 0..=mid {
+		
+                let c = hash_vec[i].clone();
+		
+		a.push(c);
+	    }
+	    for i in mid..(hash_vec.len()+1) {
+		let d = hash_vec[i].clone();
+		
+		b.push(d);
+	    }
+	} else {  //  if we've already done work on a and b ...
+	    if a.len() > 1 { // do a first
+		let mid = a.len()/2;
+	    
+		for i in 0..=mid {
+		    let c = a[i].clone();
+		
+		    a.push(c);
+	    }
+	    for i in mid..(hash_vec.len()+1) {
+		let d = hash_vec[i].clone();
+		
+		b.push(d);
+	    }
+*/
 pub fn hash_fasta_rec(
     result: &Result<fasta::Record, std::io::Error>,
     k: usize,
@@ -46,9 +86,9 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
         fasta::Reader::from_file(&filepath).unwrap();
 
     let fasta_records: Vec<Result<fasta::Record, std::io::Error>> = reader.records().collect();
-
-    let mut hash_vec: Vec<HashMap<&[u8], usize>> = fasta_records
-        .par_iter()
+    
+    let hash_vec: Vec<HashMap<&[u8], usize>> = fasta_records
+        .par_iter() //  Where you use par_iter(), instead of using map try using fold then reduce. With rayon, fold will let you merge the data into HashMaps in parallel. Then reduce will take those maps and let you merge them into a single one. If you want to avoid the Vec altogether, you should be able to call par_bridge directly on the records() result instead of calling collect (then call fold and reduce). par_bridge creates a parallel iterator from a regular iterator.
         .map(|result| hash_fasta_rec(result, k))
         .collect();
     
@@ -61,6 +101,8 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     // merging hashmaps
     //eprintln!("length of hash_vec now: {}", hash_vec.len());
     
+//  Idea was to find the longest map and somehow make that the base for comparison but scrapping for now...
+    /*
     let mut hash_len_vec = HashSet::new(); // create set of number of kmers 
     
     for h in &hash_vec {
@@ -77,7 +119,33 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     //eprintln!("this is the hash we're basing off: {:?}", final_hash);
 
     //eprintln!("length of hash_vec post removal: {}", hash_vec.len());
-
+     */
+    
+/*
+    let pair_hash = hash_vec.clone();
+    
+    let mut pairing_hash: HashMap<&[u8], usize> = HashMap::new();
+    
+    for pair in &pair_hash.into_iter().chunks(2) {
+	for p in pair {
+	    eprintln!("p.len(): {}", p.len());
+	    if pairing_hash.is_empty() {
+		let pairing_hash = p;
+		continue;
+	    } else {
+		for (kmer, freq) in p {
+		    if pairing_hash.contains_key(kmer) {
+			pairing_hash.insert(kmer, freq + pairing_hash[kmer]);
+		    } else {
+			pairing_hash.insert(kmer, freq);
+		    }
+		}
+	    }
+	}
+    }
+    eprintln!("{}", pairing_hash.len());
+     */
+    /*
     hash_vec.into_iter().for_each(|h| {
         for (kmer, freq) in h {
             if final_hash.contains_key(kmer) {
@@ -87,7 +155,38 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
             }
         }
     });
+     */
+    
 
+    eprintln!("number of hashmaps in vec: {}", hash_vec.len());
+
+    //let mut final_hash = HashMap::new();
+
+    let final_hash: HashMap<&[u8], usize> = Reduce::reduce(hash_vec.into_iter(), |mut ha, hb| {
+	    for (kmer, freq) in hb {
+		if ha.contains_key(kmer) {
+                    ha.insert(kmer, freq + ha[kmer]);
+		} else {
+                    ha.insert(kmer, freq);
+		}
+            }
+	ha
+    }).unwrap();
+    
+    //let merged: HashMap<K, V> = HashMap::new();
+	
+	/*
+	    for (kmer, freq) in pair {
+		if merged.contains_key(kmer) {
+                    merged.insert(kmer, freq + merged[kmer]);
+		} else {
+                    merged.insert(kmer, freq);
+		}
+            }
+	}
+    };
+    */
+    
     let uniq_duration = start.elapsed();
 
     eprintln!("Time elapsed merging hashmaps: {:?}\n", uniq_duration);
