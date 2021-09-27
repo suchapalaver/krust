54b40dd11212ce8888ca80ea9658979511a0a6d6
“suchapalaver” <jlivesey@gmail.com>
2021-09-27 18:29:57 -0400

    N now dealt with creating hashmap

--- src/lib.rs
+++ src/lib.rs
@@ -14,7 +14,6 @@ pub struct Config {
     pub kmer_len: usize,
     pub filepath: String,
 }
-
 impl Config {
     pub fn new(mut args: env::Args) -> Result<Config, &'static str> {
         args.next();
@@ -33,15 +32,19 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     //  Get parameters
     //  Fasta filepath
     let filepath: String = config.filepath;
+
     //  K-mer length k
     let k: usize = config.kmer_len;
 
+    // 'N'
+    let n: u8 = 78;
     //  Create fasta file reader
+
     let reader: fasta::Reader<std::io::BufReader<File>> =
         fasta::Reader::from_file(&filepath).unwrap();
 
     //  Create a DashMap
-    let fasta_hash: DashMap<Vec<u8>, u64> = DashMap::new();
+    let fasta_hash: DashMap<Vec<u8>, u128> = DashMap::new();
 
     //  Read fasta records into a Dashmap, a hashmap mutably accessible from different parallel processes
     reader
@@ -52,28 +55,30 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
             let seq: &[u8] = result.as_ref().unwrap().seq();
 
             for i in 0..(seq.len() + 1).saturating_sub(k) {
-                *fasta_hash //  Make kmer for output lexicographically min(kmer, reverse-complement)
-		    .entry(min(seq[i..i + k].to_vec(), revcomp(&seq[i..i + k])))
-		    .or_insert(0) += 1;
+                //  Irradicate kmers containing 'N'
+                if seq[i..i + k].contains(&n) {
+                } else {
+		    //  Make output kmers the lexicographically smaller of
+		    //  (kmer, reverse-complement)
+                    *fasta_hash 
+			.entry(min(seq[i..i + k].to_vec(), revcomp(&seq[i..i + k])))
+			.or_insert(0) += 1;
+                }
             }
         });
 
     //  PRINTING OUTPUT
-    //  Create handle and BufWriter for writing
+    eprintln!("Printing output ...\n");
+    //  Create handle and BufWriter for writing    
     let handle = std::io::stdout();
 
     let mut buf = BufWriter::new(handle);
 
-    fasta_hash.into_iter().for_each(|(k, f)| {
-        let kmer: String = String::from_utf8(k).unwrap();
-        //  Irradicate kmers containing 'N'
-        if kmer.contains('N') {
-        } else {
-            //  Write:
-            //  >frequency across fasta file for both kmer and its reverse complement
-            //  k-mer (lexicographically smaller of k-mer, reverse complement pair)
-            writeln!(buf, ">{}\n{}", f, kmer).expect("Unable to write data");
-        }
+    fasta_hash.into_iter().for_each(|(k, f)|{
+        //  Write:
+        //  >frequency across fasta file for both kmer and its reverse complement
+        //  k-mer (lexicographically smaller of k-mer, reverse complement pair)
+        writeln!(buf, ">{}\n{}", f, str::from_utf8(&k).unwrap()).expect("Unable to write data");
     });
     buf.flush().unwrap();
 
