a4c913d059a2540be58d5004a677dd6f8d33d7fa
“suchapalaver” <jlivesey@gmail.com>
2021-09-28 21:17:54 -0400

    u32

--- src/lib.rs
+++ src/lib.rs
@@ -38,13 +38,13 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 
     // 'N'
     let n: u8 = 78;
+    
     //  Create fasta file reader
-
     let reader: fasta::Reader<std::io::BufReader<File>> =
         fasta::Reader::from_file(&filepath).unwrap();
 
     //  Create a DashMap
-    let fasta_hash: DashMap<Vec<u8>, u128> = DashMap::new();
+    let fasta_hash: DashMap<Vec<u8>, u32> = DashMap::new();
 
     //  Read fasta records into a Dashmap, a hashmap mutably accessible from different parallel processes
     reader
@@ -55,32 +55,32 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
             let seq: &[u8] = result.as_ref().unwrap().seq();
 
             for i in 0..(seq.len() + 1).saturating_sub(k) {
+		
                 //  Irradicate kmers containing 'N'
-                if seq[i..i + k].contains(&n) {
-                } else {
+                if !seq[i..i + k].contains(&n) {
 		    //  Make output kmers the lexicographically smaller of
 		    //  (kmer, reverse-complement)
                     *fasta_hash 
 			.entry(min(seq[i..i + k].to_vec(), revcomp(&seq[i..i + k])))
 			.or_insert(0) += 1;
-                }
+		} else {}
             }
         });
 
     //  PRINTING OUTPUT
-    eprintln!("Printing output ...\n");
+    eprintln!("Printing ...\n");
+    
     //  Create handle and BufWriter for writing    
     let handle = std::io::stdout();
-
     let mut buf = BufWriter::new(handle);
 
     fasta_hash.into_iter().for_each(|(k, f)|{
         //  Write:
         //  >frequency across fasta file for both kmer and its reverse complement
         //  k-mer (lexicographically smaller of k-mer, reverse complement pair)
-        writeln!(buf, ">{}\n{}", f, str::from_utf8(&k).unwrap()).expect("Unable to write data");
+        writeln!(buf, ">{}\n{}", f, str::from_utf8(&k).unwrap()).expect("\tUnable to write data");
     });
     buf.flush().unwrap();
-
+    eprintln!("Done\n");
     Ok(())
 }
