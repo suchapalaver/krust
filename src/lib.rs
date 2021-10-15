5fc1a5ec2d68b398bd50e628b7707425024563cc
“suchapalaver” <jlivesey@gmail.com>
2021-10-14 23:28:04 -0400

    customized DashMap with FxHasher

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,35 @@
 //! # krust
 //!
-//! `krust` is a [k-mer](https://en.wikipedia.org/wiki/K-mer) counter written in Rust and run from the command line that will output canonical k-mers and their frequency across the records in a fasta file.
+//! `krust` is a [k-mer](https://en.wikipedia.org/wiki/K-mer) counter
+//! written in Rust and run from the command line that will output canonical
+//! k-mers and their frequency across the records in a fasta file.
 //!
 //! `krust` prints to `stdout`, writing, on alternate lines:  
 //! ```>{frequency}```  
 //! ```{canonical k-mer}```  
 //!
-//! `krust` uses [`rust-bio`](https://docs.rs/bio/0.38.0/bio/), [`rayon`](https://docs.rs/rayon/1.5.1/rayon/), and [`dashmap`](https://docs.rs/crate/dashmap/4.0.2).  
+//! `krust` uses [`dashmap`](https://docs.rs/crate/dashmap/4.0.2),
+//! [`rust-bio`](https://docs.rs/bio/0.38.0/bio/), [`rayon`](https://docs.rs/rayon/1.5.1/rayon/), 
+//! and [`fxhash`](https://crates.io/crates/fxhash).
 //!
-//! Run `krust` on the test data in the [`krust` Github repo](https://github.com/suchapalaver/krust), searching for kmers of length 5, like this:  
+//! Run `krust` on the test data in the [`krust` Github repo](https://github.com/suchapalaver/krust),
+//! searching for kmers of length 5, like this:  
 //! ```$ cargo run --release 5 cerevisae.pan.fa > output.tsv```  
 //! or, searching for kmers of length 21:  
 //! ```$ cargo run --release 21 cerevisae.pan.fa > output.tsv```  
 //!
-//! Future:  
-//! A function like fn single_sequence_canonical_kmers(filepath: String, k: usize) {}    
-//! Would returns k-mer counts for individual sequences in a fasta file.     
+//! Future:
+//! /// Returns k-mer counts for individual sequences in a fasta file.  
+//! ```fn single_sequence_canonical_kmers(filepath: String, k: usize) {}```      
 
 use bio::{alphabets::dna::revcomp, io::fasta};
 use dashmap::DashMap;
+use fxhash::FxHasher;
 use rayon::prelude::*;
-use std::{cmp::min, env};
+use std::{cmp::min, env, hash::BuildHasherDefault};
 
+/// This struct needs to be public as part of the way `krust` works,
+/// a simple struct for parsing the command line arguments we need.
 pub struct Config {
     pub kmer_len: usize,
     pub filepath: String,
@@ -41,16 +49,16 @@ impl Config {
     }
 }
 
+pub type KrustMap = DashMap<Box<[u8]>, u64, BuildHasherDefault<FxHasher>>;
+
 /// Reads sequences from fasta records in parallel using [`rayon`](https://docs.rs/rayon/1.5.1/rayon/).
-/// Using [`Dashmap`](https://docs.rs/dashmap/4.0.2/dashmap/struct.DashMap.html) allows updating single hashmap in parallel.
+/// Using [`Dashmap`](https://docs.rs/dashmap/4.0.2/dashmap/struct.DashMap.html) allows updating a
+/// single hashmap in parallel.
 /// Ignores substrings containing `N`.
 /// Canonicalizes by lexicographically smaller of k-mer/reverse-complement.
 /// Returns a hashmap of canonical k-mers (keys) and their frequency in the data (values).
-pub fn canonicalize_kmer(
-    filepath: String,
-    k: usize,
-) -> Result<DashMap<Box<[u8]>, u64>, &'static str> {
-    let canonical_hash: DashMap<Box<[u8]>, u64> = DashMap::new();
+pub fn canonicalize_kmers(filepath: String, k: usize) -> Result<KrustMap, &'static str> {
+    let canonical_hash: KrustMap = DashMap::with_hasher(BuildHasherDefault::<FxHasher>::default());
 
     fasta::Reader::from_file(&filepath)
         .unwrap()
@@ -63,10 +71,9 @@ pub fn canonicalize_kmer(
             for i in 0..(seq.len() + 1).saturating_sub(k) {
                 if !&seq[i..i + k].contains(&b'N') {
                     let substring: &[u8] = &seq[i..i + k];
+                    let canonical_kmer = Box::from(min(substring, &revcomp(substring)));
 
-                    *canonical_hash
-                        .entry(Box::from(min(substring, &revcomp(substring))))
-                        .or_insert(0) += 1;
+                    *canonical_hash.entry(canonical_kmer).or_insert(0) += 1;
                 } else {
                 }
             }
