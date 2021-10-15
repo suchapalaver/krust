3392d4cfb3b902488f30500c9c4c6b801c28e5d4
“suchapalaver” <jlivesey@gmail.com>
2021-10-15 11:27:01 -0400

    FxHasher annotations

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,7 @@
 //! ```{canonical k-mer}```  
 //!
 //! `krust` uses [`dashmap`](https://docs.rs/crate/dashmap/4.0.2),
-//! [`rust-bio`](https://docs.rs/bio/0.38.0/bio/), [`rayon`](https://docs.rs/rayon/1.5.1/rayon/), 
+//! [`rust-bio`](https://docs.rs/bio/0.38.0/bio/), [`rayon`](https://docs.rs/rayon/1.5.1/rayon/),
 //! and [`fxhash`](https://crates.io/crates/fxhash).
 //!
 //! Run `krust` on the test data in the [`krust` Github repo](https://github.com/suchapalaver/krust),
@@ -18,9 +18,9 @@
 //! or, searching for kmers of length 21:  
 //! ```$ cargo run --release 21 cerevisae.pan.fa > output.tsv```  
 //!
-//! Future:
-//! ```Returns k-mer counts for individual sequences in a fasta file.```  
-//! ```fn single_sequence_canonical_kmers(filepath: String, k: usize) {}```      
+//! Future:  
+//! ```fn single_sequence_canonical_kmers(filepath: String, k: usize) {}```  
+//! Returns k-mer counts for individual sequences in a fasta file.  
 
 use bio::{alphabets::dna::revcomp, io::fasta};
 use dashmap::DashMap;
@@ -48,12 +48,19 @@ impl Config {
     }
 }
 
-/// A custom `DashMap` w/ `FxHasher`.
+/// A custom `DashMap` w/ `FxHasher`.  
+///  
+/// ```use dashmap::DashMap;```  
+/// ```use fxhash::FxHasher;```  
+/// ```// skip```  
+/// ```let canonical_hash: DashFx = DashMap::with_hasher(BuildHasherDefault::<FxHasher>::default());```
+/// Useful: [Using a Custom Hash Function in Rust](https://docs.rs/hashers/1.0.1/hashers/#using-a-custom-hash-function-in-rust).
 pub type DashFx = DashMap<Box<[u8]>, u64, BuildHasherDefault<FxHasher>>;
 
 ///  - Reads sequences from fasta records in parallel using [`rayon`](https://docs.rs/rayon/1.5.1/rayon/),
-/// using [`dashmap`](https://docs.rs/dashmap/4.0.2/dashmap/struct.DashMap.html) to update  in parallel a
-/// single hashmap of canonical k-mers (keys) and their frequency in the data (values).  
+/// using a customized [`dashmap`](https://docs.rs/dashmap/4.0.2/dashmap/struct.DashMap.html)
+/// with [`FxHasher`](https://docs.rs/fxhash/0.2.1/fxhash/struct.FxHasher.html) to update in parallel a
+/// hashmap of canonical k-mers (keys) and their frequency in the data (values).  
 ///  - Ignores substrings containing `N`.  
 ///  - Canonicalizes by lexicographically smaller of k-mer/reverse-complement.  
 pub fn canonicalize_kmers(filepath: String, k: usize) -> Result<DashFx, &'static str> {
