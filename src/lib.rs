50dc94938a1a4053f3afa04e75d79d94a432eaaf
“suchapalaver” <jlivesey@gmail.com>
2022-05-07 18:08:09 -0400

    Rearrange modules in src/lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -34,30 +34,61 @@ use std::{
     io::{BufWriter, Write},
 };
 
-/// Parsing command line k-size and filepath arguments.
-pub struct Config {
-    pub kmer_len: usize,
-    pub filepath: String,
-}
+/// A custom `DashMap` w/ `FxHasher`.  
+///  
+/// ```use dashmap::DashMap;```  
+/// ```use fxhash::FxHasher;```  
+/// ```// skip```  
+/// ```let dashfx_hash: DashFx = DashMap::with_hasher(BuildHasherDefault::<FxHasher>::default());```
+/// Useful: [Using a Custom Hash Function in Rust](https://docs.rs/hashers/1.0.1/hashers/#using-a-custom-hash-function-in-rust).
+pub type DashFx = DashMap<u64, i32, BuildHasherDefault<FxHasher>>;
 
-impl Config {
-    pub fn new(mut args: env::Args) -> Result<Config, Box<dyn Error>> {
-        let kmer_len: usize = match args.nth(1) {
-            Some(arg) => match arg.parse() {
-                Ok(kmer_len) if kmer_len > 0 && kmer_len < 33 => kmer_len,
-                Ok(_) => return Err("k-mer length needs to be larger than zero and, for `krust` in its current working form, no more than 32".into()),
-                Err(_) => return Err(format!("issue with k-mer length argument: {}", arg).into()),
-            },
-            None => return Err("k-mer length input required".into()),
-        };
+///  - Reads sequences from fasta records in parallel using [`rayon`](https://docs.rs/rayon/1.5.1/rayon/),
+/// using a customized [`dashmap`](https://docs.rs/dashmap/4.0.2/dashmap/struct.DashMap.html)
+/// with [`FxHasher`](https://docs.rs/fxhash/0.2.1/fxhash/struct.FxHasher.html) to update in parallel a
+/// hashmap of canonical k-mers (keys) and their frequency in the data (values).  
+///  - Ignores substrings containing `N`.  
+///  - Canonicalizes by lexicographically smaller of k-mer/reverse-complement.  
+pub fn canonicalize_kmers(filepath: String, k: usize) -> Result<(), Box<dyn Error>> {
+    let kmer_map: DashFx = DashMap::with_hasher(BuildHasherDefault::<FxHasher>::default());
 
-        let filepath = match args.next() {
-            Some(arg) => arg,
-            None => return Err("filepath argument needed".into()),
-        };
+    let _ = fasta::Reader::from_file(&filepath)?
+        .records()
+        .into_iter()
+        .par_bridge()
+        .for_each(|r| {
+            let record = r.expect("error reading fasta record");
+            let seq: &[u8] = record.seq();
 
-        Ok(Config { kmer_len, filepath })
-    }
+            let mut i = 0;
+            while i <= seq.len() - k {
+                let sub = &seq[i..i + k];
+
+                if sub.contains(&b'N') {
+                    i += k - 1;
+                } else {
+                    let canonical_kmer = CanonKmer::new(sub);
+
+                    let bitpacked_kmer = BitpackedKmer::new(&canonical_kmer.0);
+
+                    *kmer_map.entry(bitpacked_kmer.0).or_insert(0) += 1;
+
+                    i += 1;
+                }
+            }
+        });
+
+    let mut buf = BufWriter::new(std::io::stdout());
+    kmer_map
+        .into_iter()
+        .map(|pair| UnpackedKmerData::new(pair, k).0)
+        .for_each(|(kmer, count)| {
+            writeln!(buf, ">{}\n{}", count, std::str::from_utf8(&kmer).unwrap())
+                .expect("unable to write output");
+        });
+    buf.flush()?;
+
+    Ok(())
 }
 
 /// Ascertains the [canonical k-mer](https://bioinfologics.github.io/post/2018/09/17/k-mer-counting-part-i-introduction/) of a DNA string slice.
@@ -135,59 +166,28 @@ impl UnpackedKmerData {
     }
 }
 
-/// A custom `DashMap` w/ `FxHasher`.  
-///  
-/// ```use dashmap::DashMap;```  
-/// ```use fxhash::FxHasher;```  
-/// ```// skip```  
-/// ```let dashfx_hash: DashFx = DashMap::with_hasher(BuildHasherDefault::<FxHasher>::default());```
-/// Useful: [Using a Custom Hash Function in Rust](https://docs.rs/hashers/1.0.1/hashers/#using-a-custom-hash-function-in-rust).
-pub type DashFx = DashMap<u64, i32, BuildHasherDefault<FxHasher>>;
+/// Parsing command line k-size and filepath arguments.
+pub struct Config {
+    pub kmer_len: usize,
+    pub filepath: String,
+}
 
-///  - Reads sequences from fasta records in parallel using [`rayon`](https://docs.rs/rayon/1.5.1/rayon/),
-/// using a customized [`dashmap`](https://docs.rs/dashmap/4.0.2/dashmap/struct.DashMap.html)
-/// with [`FxHasher`](https://docs.rs/fxhash/0.2.1/fxhash/struct.FxHasher.html) to update in parallel a
-/// hashmap of canonical k-mers (keys) and their frequency in the data (values).  
-///  - Ignores substrings containing `N`.  
-///  - Canonicalizes by lexicographically smaller of k-mer/reverse-complement.  
-pub fn canonicalize_kmers(filepath: String, k: usize) -> Result<(), Box<dyn Error>> {
-    let kmer_map: DashFx = DashMap::with_hasher(BuildHasherDefault::<FxHasher>::default());
+impl Config {
+    pub fn new(mut args: env::Args) -> Result<Config, Box<dyn Error>> {
+        let kmer_len: usize = match args.nth(1) {
+            Some(arg) => match arg.parse() {
+                Ok(kmer_len) if kmer_len > 0 && kmer_len < 33 => kmer_len,
+                Ok(_) => return Err("k-mer length needs to be larger than zero and, for `krust` in its current working form, no more than 32".into()),
+                Err(_) => return Err(format!("issue with k-mer length argument: {}", arg).into()),
+            },
+            None => return Err("k-mer length input required".into()),
+        };
 
-    let _ = fasta::Reader::from_file(&filepath)?
-        .records()
-        .into_iter()
-        .par_bridge()
-        .for_each(|r| {
-            let record = r.expect("error reading fasta record");
-            let seq: &[u8] = record.seq();
+        let filepath = match args.next() {
+            Some(arg) => arg,
+            None => return Err("filepath argument needed".into()),
+        };
 
-            let mut i = 0;
-            while i <= seq.len() - k {
-                let sub = &seq[i..i + k];
-
-                if sub.contains(&b'N') {
-                    i += k - 1;
-                } else {
-                    let canonical_kmer = CanonKmer::new(sub);
-
-                    let bitpacked_kmer = BitpackedKmer::new(&canonical_kmer.0);
-
-                    *kmer_map.entry(bitpacked_kmer.0).or_insert(0) += 1;
-
-                    i += 1;
-                }
-            }
-        });
-
-    let mut buf = BufWriter::new(std::io::stdout());
-    kmer_map
-        .into_iter()
-        .map(|pair| UnpackedKmerData::new(pair, k).0)
-        .for_each(|(kmer, count)| {
-            writeln!(buf, ">{}\n{}", count, std::str::from_utf8(&kmer).unwrap())
-                .expect("unable to write output");
-        });
-    buf.flush()?;
-
-    Ok(())
+        Ok(Config { kmer_len, filepath })
+    }
 }
