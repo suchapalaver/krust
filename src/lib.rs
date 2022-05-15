78c887dbea3c45ce7bf55ce8bf74cddb2813c4c9
“suchapalaver” <jlivesey@gmail.com>
2022-05-15 10:10:57 -0400

    Use `std::cmp::Ordering` instead of `<` in `CanonicalKmer::from`

--- src/lib.rs
+++ src/lib.rs
@@ -212,9 +212,12 @@ pub struct CanonicalKmer(Vec<u8>);
 impl From<(Vec<u8>, Vec<u8>)> for CanonicalKmer {
     fn from(comp: (Vec<u8>, Vec<u8>)) -> Self {
         let (reverse_complement, kmer) = (comp.0, comp.1);
-        let canonical_kmer = match reverse_complement < kmer {
-            true => reverse_complement,
-            false => kmer,
+        let canonical_kmer = match reverse_complement.cmp(&kmer) {
+            std::cmp::Ordering::Less => reverse_complement,
+            std::cmp::Ordering::Greater => kmer,
+            std::cmp::Ordering::Equal => {
+                panic!("Kmer and reverse complement can't be alphabetically equal?")
+            }
         };
         CanonicalKmer(canonical_kmer)
     }
