ab3c444f2ef9a85e45782acb3012965b328e547c
Joseph Livesey <joseph@blockchaintp.com>
2022-05-30 06:58:26 -0400

    implement `FromIterator<u8>` for `BitpackedKmer`

--- src/bitpacked_kmer.rs
+++ src/bitpacked_kmer.rs
@@ -1,6 +1,35 @@
 /// Compressing k-mers of length `0 < k < 33`, bitpacking them into unsigned integers.
 pub struct BitpackedKmer(pub u64);
 
+impl BitpackedKmer {
+    fn new() -> BitpackedKmer {
+        BitpackedKmer(0)
+    }
+
+    fn pack(&mut self, elem: u8) {
+        self.0 <<= 2;
+        let mask = match elem {
+            b'A' => 0,
+            b'C' => 1,
+            b'G' => 2,
+            b'T' => 3,
+            _ => panic!("`BitpackerKmer` handling an invalid k-mer bytestring is unexpected behavior"),
+        };
+        self.0 |= mask;
+    }
+}
+
+impl FromIterator<u8> for BitpackedKmer {
+    fn from_iter<I: IntoIterator<Item=u8>>(iter: I) -> Self {
+        let mut c = BitpackedKmer::new();
+
+        for i in iter {
+            c.pack(i);
+        }
+        c
+    }
+}
+
 impl From<&Vec<u8>> for BitpackedKmer {
     fn from(sub: &Vec<u8>) -> Self {
         let bitpacked_kmer: u64 = {
