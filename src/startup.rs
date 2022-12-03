3573336669eaa10c5b42903a0fba0cdb008f57f7
Joseph Livesey <joseph.livesey@btp.works>
2022-12-03 13:37:04 -0500

    update dashmap library

--- src/startup.rs
+++ src/startup.rs
@@ -1,8 +1,7 @@
-use crate::bitpacked_kmer::BitpackedKmer;
-use crate::dashmaps::DashFx;
-use crate::kmer::Kmer;
-use crate::revcomp_kmer::RevCompKmer;
-use crate::unpacked_kmer::UnpackedKmer;
+use super::{
+    bitpacked_kmer::BitpackedKmer, dashmaps::DashFx, kmer::Kmer, revcomp_kmer::RevCompKmer,
+    unpacked_kmer::UnpackedKmer,
+};
 use bio::io::fasta;
 use dashmap::DashMap;
 use fxhash::FxHasher;
@@ -11,20 +10,20 @@ use std::{
     collections::HashMap,
     error::Error,
     hash::BuildHasherDefault,
-    io::{BufWriter, Stdout, Write},
+    io::{stdout, BufWriter, Stdout, Write},
     path::Path,
 };
 
 pub fn run<P: AsRef<Path> + std::fmt::Debug>(path: P, k: usize) -> Result<(), Box<dyn Error>> {
-    let mut buf = BufWriter::new(std::io::stdout());
+    let mut buf = BufWriter::new(stdout());
 
     build_kmer_map(path, k)?
         .into_iter()
         .par_bridge()
-        .map(|(bitpacked_kmer, freq)| (UnpackedKmer::from_kmer_data(bitpacked_kmer, k).0, freq))
-        .map(|(unpacked_kmer, freq)| {
-            let kmer_str = String::from_utf8(unpacked_kmer).unwrap();
-            (kmer_str, freq)
+        .map(|(kmer, freq)| (UnpackedKmer::from_kmer_data(kmer, k).0, freq))
+        .map(|(kmer, freq)| {
+            let kmer = String::from_utf8(kmer).unwrap();
+            (kmer, freq)
         })
         .collect::<HashMap<String, i32>>()
         .into_iter()
@@ -46,15 +45,19 @@ fn build_kmer_map<P: AsRef<Path> + std::fmt::Debug>(
     k: usize,
 ) -> Result<DashFx, Box<dyn Error>> {
     let kmer_map: DashFx = DashMap::with_hasher(BuildHasherDefault::<FxHasher>::default());
+
     fasta::Reader::from_file(path)?
         .records()
         .into_iter()
         .par_bridge()
         .for_each(|r| {
             let record = r.expect("Error reading fasta record.");
+
             let seq: &[u8] = record.seq();
+
             process_seq(seq, &k, &kmer_map);
         });
+
     Ok(kmer_map)
 }
 
@@ -62,13 +65,17 @@ fn build_kmer_map<P: AsRef<Path> + std::fmt::Debug>(
 ///  - Canonicalizes by lexicographically smaller of k-mer/reverse-complement.
 fn process_seq(seq: &[u8], k: &usize, kmer_map: &DashFx) {
     let mut i = 0;
+
     while i <= seq.len() - k {
         let sub = &seq[i..i + k];
+
         if let Ok(kmer) = Kmer::from_substring(sub) {
             process_valid_bytes(kmer_map, kmer);
+
             i += 1;
         } else {
             let invalid_byte_index = Kmer::find_invalid_byte_index(sub);
+
             i += invalid_byte_index + 1;
         }
     }
@@ -77,6 +84,7 @@ fn process_seq(seq: &[u8], k: &usize, kmer_map: &DashFx) {
 /// Converts a valid sequence substring from a bytes string to a u64.
 fn process_valid_bytes(kmer_map: &DashFx, kmer: Kmer) {
     let BitpackedKmer(bitpacked_kmer) = kmer.0.iter().cloned().collect();
+
     // If the k-mer as found in the sequence is already a key in the `Dashmap`,
     // increment its value and move on.
     if let Some(mut freq) = kmer_map.get_mut(&bitpacked_kmer) {
@@ -84,10 +92,13 @@ fn process_valid_bytes(kmer_map: &DashFx, kmer: Kmer) {
     } else {
         // Initialize the reverse complement of this so-far unrecorded k-mer.
         let RevCompKmer(revcompkmer) = RevCompKmer::from_kmer(&kmer);
+
         // Find the alphabetically less of the k-mer substring and its reverse complement.
         let canonical_kmer = Kmer::get_canonical_kmer(revcompkmer, kmer.0);
+
         // Compress the canonical k-mer into a bitpacked 64-bit unsigned integer.
         let kmer: BitpackedKmer = canonical_kmer.0.into_iter().collect();
+
         // Add k-mer key and initial value to results.
         *kmer_map.entry(kmer.0).or_insert(0) += 1;
     }
