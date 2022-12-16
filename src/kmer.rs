f2834e69b687364e7a5984dcee1f0c6980b808be
Joseph Livesey <joseph.livesey@btp.works>
2022-12-16 16:01:03 -0500

    feat: use bytes crate

--- src/kmer.rs
+++ src/kmer.rs
@@ -1,57 +1,50 @@
+use bytes::Bytes;
+
 custom_error::custom_error! { pub ValidityError
     InvalidByte = "not a valid byte",
 }
 
 /// A valid k-mer
 #[derive(Debug, Eq, PartialEq)]
-pub struct Kmer(pub Vec<u8>);
+pub struct Kmer(pub Bytes);
 
 impl Kmer {
-    fn new() -> Self {
-        Self(Vec::new())
-    }
-
-    fn add(&mut self, elem: Monomer) {
-        self.0.push(elem.into_u8())
-    }
-
-    pub(crate) fn from_sub(sub: &[u8]) -> Result<Self, ValidityError> {
+    pub(crate) fn from_sub(sub: &Bytes) -> Result<Self, ValidityError> {
         sub.iter().map(|b| Monomer::try_from(*b)).collect()
     }
 
-    pub(crate) fn canonical(
-        reverse_complement: Vec<u8>,
-        kmer: Vec<u8>,
-    ) -> impl Iterator<Item = u8> {
-        match reverse_complement.cmp(&kmer) {
-            std::cmp::Ordering::Less => reverse_complement.into_iter(),
-            _ => kmer.into_iter(),
+    pub(crate) fn canonical<'a>(
+        reverse_complement: &'a Bytes,
+        kmer: &'a Bytes,
+    ) -> &'a Bytes {
+        match reverse_complement.cmp(kmer) {
+            std::cmp::Ordering::Less => reverse_complement,
+            _ => kmer,
         }
     }
 
-    pub(crate) fn find_invalid(sub: &[u8]) -> usize {
+    pub(crate) fn find_invalid(sub: &Bytes) -> usize {
         sub.iter()
-            .rposition(|byte| Monomer::try_from(*byte).is_err())
+            .rposition(|b| Monomer::try_from(*b).is_err())
             .unwrap()
+    }
+
+    pub(crate) fn bytes(&self) -> &Bytes {
+        &self.0
     }
 }
 
 impl FromIterator<u8> for Kmer {
     fn from_iter<I: IntoIterator<Item = u8>>(iter: I) -> Self {
-        let mut k = Kmer::new();
-
-        for i in iter {
-            k.add(Monomer::from_u8(i));
-        }
-        k
+        Self(iter.into_iter().collect())
     }
 }
 
 pub(crate) enum Monomer {
-    A(u8),
-    C(u8),
-    G(u8),
-    T(u8),
+    A,
+    C,
+    G,
+    T,
 }
 
 impl TryFrom<u8> for Monomer {
@@ -59,10 +52,10 @@ impl TryFrom<u8> for Monomer {
 
     fn try_from(value: u8) -> Result<Self, Self::Error> {
         match value {
-            b'A' => Ok(Self::A(value)),
-            b'C' => Ok(Self::C(value)),
-            b'G' => Ok(Self::G(value)),
-            b'T' => Ok(Self::T(value)),
+            b'A' => Ok(Self::A),
+            b'C' => Ok(Self::C),
+            b'G' => Ok(Self::G),
+            b'T' => Ok(Self::T),
             _ => Err(ValidityError::InvalidByte),
         }
     }
@@ -70,12 +63,18 @@ impl TryFrom<u8> for Monomer {
 
 impl FromIterator<Monomer> for Kmer {
     fn from_iter<I: IntoIterator<Item = Monomer>>(iter: I) -> Self {
-        let mut k = Self::new();
+        Self(iter.into_iter().map(Monomer::into_u8).collect())
+    }
+}
 
-        for i in iter {
-            k.add(i);
+impl From<u64> for Monomer {
+    fn from(u: u64) -> Self {
+        match u {
+            0 => Self::A,
+            1 => Self::C,
+            2 => Self::G,
+            _ => Self::T,
         }
-        k
     }
 }
 
@@ -83,10 +82,10 @@ impl FromIterator<Monomer> for Kmer {
 impl Into<u64> for Monomer {
     fn into(self) -> u64 {
         match self {
-            Self::A(_) => 0,
-            Self::C(_) => 1,
-            Self::G(_) => 2,
-            Self::T(_) => 3,
+            Self::A => 0,
+            Self::C => 1,
+            Self::G => 2,
+            Self::T => 3,
         }
     }
 }
@@ -94,37 +93,28 @@ impl Into<u64> for Monomer {
 impl Monomer {
     fn complement(self) -> Self {
         match self {
-            Self::A(_) => Self::from_u8(b'T'),
-            Self::C(_) => Self::from_u8(b'G'),
-            Self::G(_) => Self::from_u8(b'C'),
-            Self::T(_) => Self::from_u8(b'A'),
+            Self::A => Self::T,
+            Self::C => Self::G,
+            Self::G => Self::C,
+            Self::T => Self::A,
         }
     }
 
     fn from_u8(byte: u8) -> Self {
         match byte {
-            b'A' => Self::A(byte),
-            b'C' => Self::C(byte),
-            b'G' => Self::G(byte),
-            _ => Self::T(byte),
+            b'A' => Self::A,
+            b'C' => Self::C,
+            b'G' => Self::G,
+            _ => Self::T,
         }
     }
 
     fn into_u8(self) -> u8 {
         match self {
-            Self::A(b) => b,
-            Self::C(b) => b,
-            Self::G(b) => b,
-            Self::T(b) => b,
-        }
-    }
-
-    fn unpack(bit: u64) -> Self {
-        match bit {
-            0 => Self::from_u8(b'A'),
-            1 => Self::from_u8(b'C'),
-            2 => Self::from_u8(b'G'),
-            _ => Self::from_u8(b'T'),
+            Self::A => b'A',
+            Self::C => b'C',
+            Self::G => b'G',
+            Self::T => b'T',
         }
     }
 }
@@ -152,9 +142,9 @@ impl Bitpack {
         Self(0)
     }
 
-    fn pack(&mut self, elem: u8) {
+    fn pack(&mut self, elem: &u8) {
         self.shift();
-        let mask: u64 = Monomer::from_u8(elem).into();
+        let mask: u64 = Monomer::from_u8(*elem).into();
         self.0 |= mask
     }
 
@@ -163,36 +153,37 @@ impl Bitpack {
     }
 }
 
-impl FromIterator<u8> for Bitpack {
-    fn from_iter<I: IntoIterator<Item = u8>>(iter: I) -> Self {
-        let mut c = Self::new();
+impl From<&Bytes> for Bitpack {
+    fn from(bytes: &Bytes) -> Self {
+        let mut packed = Self::new();
+        bytes.iter().for_each(|b| {
+            packed.pack(b)
+        });
+        packed
+    }
+}
 
-        for i in iter {
-            c.pack(i)
-        }
-        c
+impl From<&Kmer> for Bitpack {
+    fn from(kmer: &Kmer) -> Self {
+        let mut packed = Self::new();
+        kmer.bytes().iter().for_each(|b| {
+            packed.pack(b)
+        });
+        packed
     }
 }
 
 /// Unpack bitpacked k-mer data
 #[derive(Hash, PartialEq, Eq)]
-pub struct Unpack(pub Vec<u8>);
+pub struct Unpack(pub Bytes);
 
 impl Unpack {
-    fn new() -> Self {
-        Unpack(Vec::new())
-    }
-
-    fn add(&mut self, elem: u8) {
-        self.0.push(elem);
-    }
-
     pub(crate) fn bit(bit: u64, k: usize) -> Self {
         (0..k)
             .into_iter()
             .map(|i| bit.isolate(i, k))
             .map(|bit| bit.replace())
-            .map(Monomer::unpack)
+            .map(Monomer::from)
             .map(|m| m.into_u8())
             .collect()
     }
@@ -200,38 +191,20 @@ impl Unpack {
 
 impl FromIterator<u8> for Unpack {
     fn from_iter<I: IntoIterator<Item = u8>>(iter: I) -> Self {
-        let mut c = Unpack::new();
-
-        for i in iter {
-            c.add(i)
-        }
-        c
+        Self(iter.into_iter().collect())
     }
 }
 
 /// Convert a DNA string slice into its [reverse compliment](https://en.wikipedia.org/wiki/Complementarity_(molecular_biology)#DNA_and_RNA_base_pair_complementarity).
-pub(crate) struct RevComp(pub Vec<u8>);
+pub(crate) struct RevComp(pub Bytes);
 
 impl FromIterator<u8> for RevComp {
     fn from_iter<I: IntoIterator<Item = u8>>(iter: I) -> Self {
-        let mut c = RevComp::new();
-
-        for i in iter {
-            c.add(i)
-        }
-        c
+        Self(iter.into_iter().collect())
     }
 }
 
 impl RevComp {
-    fn new() -> Self {
-        Self(vec![])
-    }
-
-    fn add(&mut self, elem: u8) {
-        self.0.push(elem)
-    }
-
     pub(crate) fn from_kmer(kmer: &Kmer) -> Self {
         kmer.0
             .iter()
@@ -248,8 +221,8 @@ pub mod test {
     #[test]
     fn test_from_valid_substring() {
         let sub = &[b'G', b'A', b'T', b'T', b'A', b'C', b'A'];
-        let k = Kmer::from_sub(sub).unwrap();
-        insta::assert_snapshot!(format!("{:?}", k), @"Kmer([71, 65, 84, 84, 65, 67, 65])");
+        let k = Kmer::from_sub(&Bytes::copy_from_slice(sub)).unwrap();
+        insta::assert_snapshot!(format!("{:?}", k), @r###"Kmer(b"GATTACA")"###);
     }
 
     #[test]
@@ -261,34 +234,34 @@ pub mod test {
     #[test]
     fn from_substring_returns_err_for_invalid_substring() {
         let sub = &[b'N'];
-        let k = Kmer::from_sub(sub);
+        let k = Kmer::from_sub(&Bytes::copy_from_slice(sub));
         assert!(k.is_err());
     }
 
     #[test]
     fn find_invalid_works() {
         let dna = "NACNN".as_bytes();
-        let ans = Kmer::find_invalid(dna);
+        let ans = Kmer::find_invalid(&Bytes::copy_from_slice(dna));
         assert_eq!(4, ans);
         assert_eq!(&b'N', dna.iter().collect::<Vec<_>>()[ans]);
 
         let dna = "NACNG".as_bytes();
-        let ans = Kmer::find_invalid(dna);
+        let ans = Kmer::find_invalid(&Bytes::copy_from_slice(dna));
         assert_eq!(3, ans);
         assert_eq!(&b'N', dna.iter().collect::<Vec<_>>()[ans]);
 
         let dna = "NANTG".as_bytes();
-        let ans = Kmer::find_invalid(dna);
+        let ans = Kmer::find_invalid(&Bytes::copy_from_slice(dna));
         assert_eq!(2, ans);
         assert_eq!(&b'N', dna.iter().collect::<Vec<_>>()[ans]);
 
         let dna = "NNCTG".as_bytes();
-        let ans = Kmer::find_invalid(dna);
+        let ans = Kmer::find_invalid(&Bytes::copy_from_slice(dna));
         assert_eq!(1, ans);
         assert_eq!(&b'N', dna.iter().collect::<Vec<_>>()[ans]);
 
         let dna = "NACTG".as_bytes();
-        let ans = Kmer::find_invalid(dna);
+        let ans = Kmer::find_invalid(&Bytes::copy_from_slice(dna));
         assert_eq!(0, ans);
         assert_eq!(&b'N', dna.iter().collect::<Vec<_>>()[ans]);
     }
