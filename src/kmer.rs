4eaeeeff6ddcd9068202577008e2b619819c71d3
Joseph Livesey <joseph.livesey@btp.works>
2023-01-01 11:32:19 -0500

    refactor(kmer): byteconversion methods

--- src/kmer.rs
+++ src/kmer.rs
@@ -26,7 +26,7 @@ impl Kmer {
     pub(crate) fn pack(&mut self) {
         for elem in self.bytes.iter() {
             self.packed_bits <<= 2;
-            let mask: u64 = u8::into_u64(elem);
+            let mask: u64 = ByteConversion::into_u64(elem);
             self.packed_bits |= mask
         }
     }
@@ -36,7 +36,7 @@ impl Kmer {
             .bytes
             .iter()
             .rev()
-            .map(u8::complement)
+            .map(ByteConversion::reverse_complement)
             .collect::<Bytes>()
         {
             reverse_complement if reverse_complement.cmp(&self.bytes) == Ordering::Less => {
@@ -51,7 +51,7 @@ impl Kmer {
         self.bytes = (0..k)
             .into_iter()
             .map(|i| self.packed_bits << ((i * 2) + 64 - (k * 2)) >> 62)
-            .map(u8::from_u64)
+            .map(ByteConversion::from_u64)
             .collect()
     }
 }
@@ -65,14 +65,10 @@ impl FromIterator<u8> for Kmer {
     }
 }
 
-trait ByteConversions {
-    fn complement(u: &Self) -> Self;
-    fn from_u64(u: u64) -> Self;
-    fn into_u64(u: &Self) -> u64;
-}
+struct ByteConversion;
 
-impl ByteConversions for u8 {
-    fn complement(u: &Self) -> Self {
+impl ByteConversion {
+    fn reverse_complement(u: &u8) -> u8 {
         match *u {
             b'A' => b'T',
             b'C' => b'G',
@@ -81,7 +77,7 @@ impl ByteConversions for u8 {
         }
     }
 
-    fn from_u64(u: u64) -> Self {
+    fn from_u64(u: u64) -> u8 {
         match u {
             0 => b'A',
             1 => b'C',
@@ -90,7 +86,7 @@ impl ByteConversions for u8 {
         }
     }
 
-    fn into_u64(u: &Self) -> u64 {
+    fn into_u64(u: &u8) -> u64 {
         match *u {
             b'A' => 0,
             b'C' => 1,
@@ -121,23 +117,23 @@ pub mod test {
     #[test]
     fn from_sub_finds_invalid_byte_index() {
         let dna = "NACNN".as_bytes();
-        let ans = Kmer::from_sub(Bytes::copy_from_slice(dna));
-        assert_eq!(Err(0), ans);
+        let res = Kmer::from_sub(Bytes::copy_from_slice(dna));
+        assert_eq!(Err(0), res);
 
         let dna = "ANCNG".as_bytes();
-        let ans = Kmer::from_sub(Bytes::copy_from_slice(dna));
-        assert_eq!(Err(1), ans);
+        let res = Kmer::from_sub(Bytes::copy_from_slice(dna));
+        assert_eq!(Err(1), res);
 
         let dna = "AANTG".as_bytes();
-        let ans = Kmer::from_sub(Bytes::copy_from_slice(dna));
-        assert_eq!(Err(2), ans);
+        let res = Kmer::from_sub(Bytes::copy_from_slice(dna));
+        assert_eq!(Err(2), res);
 
         let dna = "CCCNG".as_bytes();
-        let ans = Kmer::from_sub(Bytes::copy_from_slice(dna));
-        assert_eq!(Err(3), ans);
+        let res = Kmer::from_sub(Bytes::copy_from_slice(dna));
+        assert_eq!(Err(3), res);
 
         let dna = "AACTN".as_bytes();
-        let ans = Kmer::from_sub(Bytes::copy_from_slice(dna));
-        assert_eq!(Err(4), ans);
+        let res = Kmer::from_sub(Bytes::copy_from_slice(dna));
+        assert_eq!(Err(4), res);
     }
 }
