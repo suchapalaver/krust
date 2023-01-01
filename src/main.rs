3c525455cafcbff1c7d74bd7a7b546eed4701b43
Joseph Livesey <joseph.livesey@btp.works>
2023-01-01 11:22:27 -0500

    style(main): make terminal messages blue

--- src/main.rs
+++ src/main.rs
@@ -37,13 +37,13 @@ fn main() {
     let config = Config::new(k, path, reader).unwrap_or_else(|e| {
         eprintln!(
             "{}\n {}",
-            "Problem parsing arguments:".red().bold(),
-            e.to_string().red()
+            "Problem parsing arguments:".blue().bold(),
+            e.to_string().blue()
         );
         eprintln!();
         eprintln!(
             "{}\n {}\n  {}\n   {}",
-            "Help menu:".green().bold(),
+            "Help menu:".blue().bold(),
             "$ cargo run -- --help".bold(),
             "or".underline(),
             "$ krust --help".bold()
@@ -52,16 +52,16 @@ fn main() {
         process::exit(1);
     });
 
-    eprintln!("{}: {}", "k-length".bold(), k.yellow().bold());
-    eprintln!("{}: {}", "data".bold(), path.underline().bold().yellow());
-    eprintln!("{}: {}", "reader".bold(), reader.yellow().bold());
+    eprintln!("{}: {}", "k-length".bold(), k.blue().bold());
+    eprintln!("{}: {}", "data".bold(), path.underline().bold().blue());
+    eprintln!("{}: {}", "reader".bold(), reader.blue().bold());
     eprintln!();
 
     if let Err(e) = run::run(config.path, config.k, config.reader) {
         eprintln!(
             "{}\n {}",
-            "Application error:".red().bold(),
-            e.to_string().red()
+            "Application error:".blue().bold(),
+            e.to_string().blue()
         );
         drop(e);
         process::exit(1);
